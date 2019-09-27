d761a296e06906a8f86c3ac29c8a082418bdd5ce
Tammy Gu <tagu@berkeley.edu>
2019-09-27 09:52:45 -0700

    another contact retry
    change email on contact.php, changed sizing of work description boxes for responsiveness

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'info@tammygu.com';
+$from = 'tagu@berkeley.edu';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'tagu@berkeley.edu';
@@ -73,3 +73,5 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 else {
     echo $responseArray['message'];
 }
+
+?>
