3ff65af21627ae7c187c15f52fe42ab5c5909701
Tammy Gu <tagu@berkeley.edu>
2019-09-29 09:45:55 -0700

    Update contact.php
    update php test

--- contact.php
+++ contact.php
@@ -1,5 +1,19 @@
 <?php
-  
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'info@tammygu.com';
+  $email_subject = 'New Message From Your Portfolio'
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+  $to = "tagu@berkeley.edu";
+  $headers = "From: $email_from \r\n";
+  $headers . = "Reply-To: $visitor_email \r\n";
+  mail($to,$email_subject,$email_body,$headers);
+  header("Location: index.html");
 
 
 ?>
